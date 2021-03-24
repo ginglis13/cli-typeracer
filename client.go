d66267b2f6432376694a9167240a3dcfb714b836
ginglis13 <ginglis05@gmail.com>
2021-03-24 00:03:14 -0400

    Move from HTTP server to websocket connection for streaming communication real time

--- client.go
+++ client.go
@@ -1,88 +1,63 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
 package main
 
 import (
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
 	"fmt"
+	"strings"
+
 	"github.com/eiannone/keyboard"
 	"github.com/fatih/color"
-	"net/http"
-	"encoding/json"
-	"log"
-	"bytes"
-	"io/ioutil"
-	"strings"
-	"flag"
+    "github.com/ginglis13/cli-typeracer/models"
+	"github.com/gorilla/websocket"
 )
 
-type GameState struct {
-	ID int `json:"ID"`
-	Over bool `json:"Over"`
-	Clients map[string]ClientState `json:"Clients"` // take length to verify max of 4 participants
-	String []byte `json:"String"` // the string/paragraph to type
-	Winner []byte `json:"Winner"`
-	StrLen int `json:"StrLen"`
-	// also use the progress attribute to check against other players
+var addr = flag.String("addr", "localhost:8888", "http service address")
+
+type ClientCliArgs struct {
+	nick string
+	host string
+	port int
+	gameID int
 }
 
-type ClientState struct {
-	UserID string `json:"UserID"`
-	GameID int  `json:"GameID"`
-	Progress int `json:"Progress"` // length of correct input to show comparison to other players
-	UserInput string `json:"UserInput"`
-	Complete bool `json:"Complete"` // indicates client has finished the input
-	IsLeader bool `json:"IsLeader"`
-	WPM float64    `json:"WPM"`
+func parseArgs() ClientCliArgs {
+	args := ClientCliArgs{}
+
+	flag.IntVar(&args.gameID, "g", 0, "Join game by game id")
+	flag.StringVar(&args.nick, "n", "", "Set nickname")
+	flag.StringVar(&args.host, "host", "localhost", "Host address/domain of game")
+	flag.IntVar(&args.port, "p", 8880, "Host port")
+	flag.Parse()
+	log.SetFlags(0)
+
+	fmt.Println("CLI Typeracer")
+	fmt.Println("Press ESC to quit")
+
+	fmt.Printf("Host is set to %v:%v\n", args.host, args.port)
+
+	if args.gameID == 0 {
+		fmt.Println("Enter the ID of the game you'd like to join, or enter -1 to start a new game:")
+		fmt.Scanf("%d", &args.gameID)
+	}
+
+	if args.nick == "" {
+		fmt.Println("Enter a nickname to join the game with:")
+		fmt.Scanf("%s", &args.nick)
+	}
+
+	return args
 }
-
-// Display Game Over Message, Winner, WPM, menu options to start over
-func gameOver(gs *GameState) {
-	fmt.Print("\033[H\033[2J")
-	fmt.Println(strings.Repeat("#", 8), "GAME OVER", strings.Repeat("#", 8))
-	fmt.Printf("Winner:\t")
-	color.Green(string(*&gs.Winner))
-	fmt.Println("Player WPM: ")
-	finalGameState := *&gs.Clients
-	// Print WPM
-	for client, state := range finalGameState {
-		fmt.Printf("%10s: %.3v WPM\n", client, state.WPM)
-	}
-
-}
-
-func sendState(c *ClientState, host string, port int) *GameState {
-
-	bytesRepresentation, err := json.Marshal(&c)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	server := fmt.Sprintf("http://%s:%v/typeracer", host, port)
-	resp, err := http.Post(server, "application/json", bytes.NewBuffer(bytesRepresentation))
-	if err != nil {
-		log.Fatalln(err)
-
-	}
-
-	body, readErr := ioutil.ReadAll(resp.Body)
-	if readErr != nil {
-		log.Fatalln(readErr)
-	}
-
-	gs := GameState{c.GameID, false, make(map[string]ClientState), []byte(""), []byte(""), 0}
-
-	jErr := json.Unmarshal(body, &gs)
-	if jErr != nil {
-		log.Fatalln(jErr)
-	}
-
-	fmt.Println("GS: ", gs)
-
-	// Set Game ID From Server Response
-	*&c.GameID = gs.ID
-
-	return &gs
-
-}
-
 
 func delInput(chars []int32) []int32{
 	if len(chars) > 0 {
@@ -125,104 +100,12 @@ func checkInput(chars []int32, s []byte) bool {
 		color.Red(cs)
 	}
 	return res
-
 }
 
-func beginGame(c *ClientState, host string, port int) *GameState {
-	beginGame := map[string]interface{}{
-		"userID": c.UserID,
-		"gameID": c.GameID,
-	}
-
-	fmt.Println("BEGINNING GAME WITH ID ", c.GameID)
-
-	bytesRepresentation, err := json.Marshal(beginGame)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	server := fmt.Sprintf("http://%s:%v/typeracer", host, port)
-	resp, err := http.Post(server, "application/json", bytes.NewBuffer(bytesRepresentation))
-	fmt.Println("RESPONSE:", resp)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	body, readErr := ioutil.ReadAll(resp.Body)
-	if readErr != nil {
-		log.Fatalln(readErr)
-	}
-
-	gs := GameState{c.GameID, false, make(map[string]ClientState), []byte(""), []byte(""), 0}
-
-	jErr := json.Unmarshal(body, &gs)
-	if jErr != nil {
-		log.Fatalln(jErr)
-	}
-
-	fmt.Println(gs)
-
-	// Set Game ID From Server Response
-	*&c.GameID = gs.ID
-
-	return &gs
-}
-
-
-func main() {
-
-	/* Parse Args */
-	var nick, host string
-	var port, gameID int
-
-	flag.IntVar(&gameID, "g", 0, "Join game by game id")
-	flag.StringVar(&nick, "n", "", "Set nickname")
-	flag.StringVar(&host, "host", "localhost", "Host address/domain of game")
-	flag.IntVar(&port, "p", 8080, "Host port")
-	flag.Parse()
-
-	fmt.Println("CLI Typeracer")
-	fmt.Println("Press ESC to quit")
-
-	fmt.Printf("Host is set to %v:%v\n", host, port)
-
-	if gameID == 0 {
-		fmt.Println("Enter the ID of the game you'd like to join, or enter -1 to start a new game:")
-		fmt.Scanf("%d", &gameID)
-	}
-
-	if nick == "" {
-		fmt.Println("Enter a nickname to join the game with:")
-		fmt.Scanf("%s", &nick)
-	}
-
-	c := ClientState{nick, gameID, 0, "", false, false, 0}
-
-	fmt.Println("Sending state", c)
-
-	/* return new gameID if -1 specified */
-	gs := beginGame(&c, host, port)
-	c.GameID = *&gs.ID
-	s := *&gs.String
-
-	/* Open Keyboard */
-	err := keyboard.Open()
-	if err != nil {
-		panic(err)
-	}
-	defer keyboard.Close()
-
+func keyboardInput(clientState *models.ClientState, inp chan struct{}) {
+	defer close(inp) 
 	chars := make([]int32, 0)
-
-	//_s := "test string."
-
-	//s := []byte(_s)
-
 	for {
-		/* clear screen, print prompt */
-		//fmt.Print("\033[H\033[2J")
-		fmt.Println("Quote:", string(s))
-		res := checkInput(chars, s)
-
 		char, key, err := keyboard.GetKey()
 		if err != nil {
 			panic(err)
@@ -234,26 +117,152 @@ func main() {
 		} else {
 			chars = append(chars, char)
 		}
+		clientState.UserInput = string(chars)
+	}
+}
 
-		if res && len(chars) == len(s){
-			fmt.Println("Game over.")
-			c.Complete = true
-			//sendState(&c, host, port)
-			//break
+func recvGameState(conn *websocket.Conn, clientState *models.ClientState, done chan bool) {
+	//done := make(chan struct{})
+	//defer close(done)
+	for {
+		var gs models.GameState
+		err := conn.ReadJSON(&gs)
+		if err != nil {
+			log.Printf("Error reading receiver json")
+		}
+		s := gs.String
+		log.Printf("recv: %s", gs)
+		res := checkInput([]int32(clientState.UserInput), s)
+		if res && len(clientState.UserInput) == len(s) {
+			clientState.Complete = true
 		}
 
-		c.UserInput = string(chars)
-		fmt.Println("SENDING TO GAME",c.GameID)
-		game := sendState(&c, host, port)
-		//c.GameID = game.ID.(int)
-		c.GameID = *&game.ID
-		fmt.Println("GAME:", c.GameID)
-		fmt.Println("GAME:", *&game.Over)
-		fmt.Println("GAME:", *&game)
-		if *&game.Over == true {
-			fmt.Println("GAME OVER")
-			gameOver(game)
+		// Check if server registered them as leader
+		if gs.Clients[clientState.UserID].IsLeader {
+			clientState.IsLeader = true
+		}
+
+		log.Printf("GAME OVER STATUS: %v", gs.Over)
+
+		if gs.Over {
+			gameOver(&gs)
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			done<-true
 			break
+		}
+	}
+
+}
+
+func gameOver(gs *models.GameState) {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println(strings.Repeat("#", 8), "GAME OVER", strings.Repeat("#", 8))
+	fmt.Printf("Winner:\t")
+	color.Green(string(*&gs.Winner))
+	fmt.Println("Player WPM: ")
+	finalGameState := *&gs.Clients
+	// Print WPM
+	for client, state := range finalGameState {
+		fmt.Printf("%10s: %.3v WPM\n", client, state.WPM)
+	}
+
+
+	/* TODO: allow player to play again with same players
+	if cs.IsLeader {
+		fmt.Println("You are the game leader.")
+		fmt.Println("Would you like to play again with the same players? [y/N]")
+		var s string
+		fmt.Scanf("%s", s)
+		fmt.Println(s)
+	}
+	*/
+
+}
+
+func main() {
+	/* Parse Args */
+	args := parseArgs()
+
+	/* Initial Client State */
+	clientState := models.ClientState{args.nick, args.gameID, 0, "", false, false, 0}
+
+	log.Println("Sending state", clientState)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	host := fmt.Sprintf("%s:%v", args.host, args.port)
+
+	u := url.URL{Scheme: "ws", Host: host, Path: "/echo"}
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	log.Printf("TYPE OF c: %T", c)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	//done := make(chan struct{})
+	done := make(chan bool, 1)
+	defer close(done)
+
+	/* For receiving game state data from server */
+	go recvGameState(c, &clientState, done)
+
+	/* Open Keyboard and receive input in background goroutine */
+	keyErr := keyboard.Open()
+	if keyErr != nil {
+		panic(keyErr)
+	}
+	defer keyboard.Close()
+
+	inp := make(chan struct{})
+	go keyboardInput(&clientState, inp)
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			err := c.WriteJSON(&clientState)
+			if err != nil {
+				log.Println("write:", err)
+				log.Println("t:", t)
+				return
+			}
+			log.Println(&clientState)
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		/* TODO : UNCOMMENT FOR REAL TIME w/o 1s delay
+		default:
+			err := c.WriteJSON(&clientState)
+			if err != nil {
+				log.Println("write:", err)
+				log.Println("t:", t)
+				return
+			}
+			log.Println(&clientState)
+		*/
 		}
 	}
 }
--- server.go
+++ server.go
@@ -1,200 +1,88 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
+    "time"
 
-	//"math/rand"
-	"time"
+    "github.com/ginglis13/cli-typeracer/models"
+	"github.com/gorilla/websocket"
 )
 
-// type ClientState struct {
-// 	UserID string
-// 	GameID int
-// 	Progress int // length of correct input to show comparison to other players
-// 	UserInput string // TODO: check input on client or server side
-// 	Complete bool // indicates client has finished the input
-// 	//ResponseWriter http.ResponseWriter
-// 	//isCreate bool // indicates that the user is the game creator - for asking if they want to start another
-// }
-type ClientState struct {
-	UserID         string `json:"UserID"`
-	GameID         int    `json:"GameID"`
-	Progress       int    `json:"Progress"` // length of correct input to show comparison to other players
-	UserInput      string `json:"UserInput"`
-	Complete       bool   `json:"Complete"` // indicates client has finished the input
-	// Used for beginning the game - leader will indicate all players have joined, will be asked to re run game
-	IsLeader	   bool   `json:"IsLeader"` 
-	WPM 		   float64    `json:"WPM"`
-	ResponseWriter http.ResponseWriter
-}
+var addr = flag.String("addr", "localhost:8880", "http service address")
 
-type GameState struct {
-	ID      int `json:"ID"`
-	Over    bool `json:"Over"`
-	Clients map[string]*ClientState `json:"Clients"` // take length to verify max of 4 participants
-	String  []byte `json:"String"`                // the string/paragraph to type
-	Winner []byte  `json:"Winner"`
-	StartTime	time.Time `json:"StartTime"`
-	StrLen int `json:"StrLen"`
-	// also use the progress attribute to check against other players
-}
 
-type GameMap map[int]*GameState
+var upgrader = websocket.Upgrader{} // use default options
 
-var GAMES = make(GameMap) // keep track of games in map TODO allow for multiple games at a time
+var gameState = models.GameState{0, false, make(map[string]*models.ClientState), []byte("test string."), []byte(""), time.Now() /*TODO*/, 2}
 
-/*
-func joinGame(games map[int]GameState, gameID int){
-	exists, gs = games[gameID]
-	if exists {
-		if len(gs) < 4{
-			// ... connect client to game
-		}
-	}
-}
-*/
-func game(w http.ResponseWriter, req *http.Request, gs *GameState) {
-	// need to write an interface for HandleFunc to pass vars
-
-}
-
-/* UNCOMMENT FOR REAL DEAL
-func getQuote() []byte {
-	quoteNum := rand.Intn(10)
-	quotePath := fmt.Sprintf("quotes/%v.txt", quoteNum)
-	data, err := ioutil.ReadFile(quotePath)
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		panic(err)
+		log.Print("upgrade:", err)
+		return
 	}
+	defer c.Close()
+	for {
+        var clientState models.ClientState
+		err := c.ReadJSON(&clientState)
+        if err != nil {
+            log.Println("[ERROR] Unable to read JSON from client.")
+        }
 
-	return data
-}
-*/
+		log.Printf("[CLIENT STATE]: %s", clientState)
 
-func continueMenu() {
+        // Add client to list of clients and set as leader if they are first client
+        if len(gameState.Clients) == 0 {
+            clientState.IsLeader = true
+        }
 
-}
+        gameState.Clients[clientState.UserID] = &clientState
 
-func typeracer(w http.ResponseWriter, req *http.Request) {
-	_s := "test string."
-	s := []byte(_s)
-	quoteLen := len(strings.Fields(_s)) // Number of Words in Quote, used for calcs
+        if clientState.Complete {
+            elapsedTime := time.Now().Sub(gameState.StartTime).Seconds()
+            log.Printf("****** ELAPSED TIME %v *******\n", elapsedTime)
+            log.Printf("****** WORD COUNT %v *******\n", gameState.StrLen)
+            log.Printf("****** WPM %.3f *******\n", float64(gameState.StrLen)/elapsedTime*60.0)
 
-	log.Printf("%s %s /typeracer from %s", req.Method, req.Proto, req.RemoteAddr)
-
-	// read incoming json request
-	//var cs ClientState
-	// err := json.NewDecoder(req.Body).Decode(&cs)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// 	return
-	// }
-
-	body, readErr := ioutil.ReadAll(req.Body)
-	if readErr != nil {
-		log.Fatalln(readErr)
-	}
-
-	cs := ClientState{}
-
-	jErr := json.Unmarshal(body, &cs)
-	if jErr != nil {
-		log.Fatalln(jErr)
-	}
-
-	cs.ResponseWriter = w
-	fmt.Println(cs)
-
-	// Check if the specified gameID is in map of Games
-	var gs *GameState
-	var isset bool
-	if gs, isset = GAMES[cs.GameID]; isset == true {
-		// If Game is complete, send end state to all clients in the Game
-		if cs.Complete {
-
-			elapsedTime := time.Now().Sub(GAMES[cs.GameID].StartTime).Seconds()
-			fmt.Printf("****** ELAPSED TIME %v *******\n", elapsedTime)
-			fmt.Printf("****** WORD COUNT %v *******\n", GAMES[cs.GameID].StrLen)
-			fmt.Printf("****** WPM %.3f *******\n", float64(GAMES[cs.GameID].StrLen)/elapsedTime*60.0)
+            clientState.WPM = float64(gameState.StrLen)/elapsedTime*60.0
 
 
-			if len(GAMES[cs.GameID].Winner) == 0 { // Don't reset winner if someone else finishes
-				GAMES[cs.GameID].Winner = []byte(cs.UserID)
-			}
+            if len(gameState.Winner) == 0 { // Don't reset winner if someone else finishes
+                gameState.Winner = []byte(clientState.UserID)
+            }
 
-			GAMES[cs.GameID].Over = true
-			gs.Over = true
-			// Write game over to all clients in game (but the incoming client so as not to close connection)
-			for _, client := range GAMES[cs.GameID].Clients {
-				client.WPM = float64(GAMES[cs.GameID].StrLen)/elapsedTime*60.0
-				if client.UserID == cs.UserID {
-					continue
-				}
-				client.Complete = true
-				json.NewEncoder(client.ResponseWriter).Encode(GAMES[cs.GameID])
-				fmt.Println("GAME OVER SENT TO ", client.UserID)
+            gameState.Over = true
+            // Write game over to all clients in game (but the incoming client so as not to close connection)
+            for _, client := range gameState.Clients {
+                client.WPM = float64(gameState.StrLen)/elapsedTime*60.0
+                if client.UserID == clientState.UserID {
+                    continue
+                }
+                client.Complete = true
+                log.Println("GAME OVER SENT TO ", client.UserID)
+            }
+        }
 
-			}
-			// Write to this incoming clientk
-			json.NewEncoder(w).Encode(GAMES[cs.GameID])
-			//delete(GAMES, cs.GameID)
-			return
-		}
-		//cs.ResponseWriter = w
-		gs.Clients[cs.UserID] = &cs
-		fmt.Println("CLIENTS:", gs.Clients)
-	} else { // new game, TODO search for an open game (10 max)
-		cs.ResponseWriter = w
-		cs.GameID = 10
-		gs = &GameState{cs.GameID, false, make(map[string]*ClientState),s /*getQuote() TODO UNCOMMENT AFTER TESTING*/, nil, time.Now(), quoteLen} 
-		fmt.Println("gs", gs)
-		gs.Clients[cs.UserID] = &cs
-		GAMES[cs.GameID] = gs
-		fmt.Println("CLIENTS:", gs.Clients)
-	}
+		log.Printf("[GAME STATE]: %s", gameState)
+	    err = c.WriteJSON(&gameState)
+        if err != nil {
+            log.Println("[ERROR] Unable to write JSON back to client.")
+        }
 
-	log.Printf("Client Request JSON: %v", cs)
-
-	log.Printf("Server Response JSON: %v", gs)
-	log.Printf("GAME: %v", *GAMES[cs.GameID])
-
-	// write back to client
-	json.NewEncoder(w).Encode(*GAMES[cs.GameID])
-	
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+    }
 }
 
 func main() {
-	// TODO UNCOMMENT AFTER TESTING
-	//rand.Seed(time.Now().UnixNano())
-
-	/* Parse Args */
-	port := flag.Int("p", 8080, "Host port")
-	/*
-	   f, _ := os.Create("/var/log/golang/golang-server.log")
-	   defer f.Close()
-	   log.SetOutput(f)
-	*/
-
 	flag.Parse()
-
-	http.HandleFunc("/typeracer", typeracer)
-	http.HandleFunc("/headers", headers)
-
-	p := fmt.Sprintf(":%v", *port)
-	log.Printf("Listening on port %v", *port)
-	http.ListenAndServe(p, nil)
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
